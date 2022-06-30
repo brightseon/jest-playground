9dfb95e6daf3b46956808458686f82f49b238f6e
brightseon <public.seon@gmail.com>
2022-06-30 22:59:03 +0900

    feat: Using Multiple Expect Statements

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -2,6 +2,20 @@ function Triangle(side1, side2, side3) {
     this.side1 = side1;
     this.side2 = side2;
     this.side3 = side3;
+    this.isTriangle =
+        this.side1 > this.side2 + this.side3 ||
+        this.side2 > this.side1 + this.side3 ||
+        this.side3 > this.side1 + this.side2
+            ? 'not a triangle'
+            : 'this is a triangle';
+    this.isScalene =
+        this.side1 !== this.side2 && this.side1 !== this.side3 && this.side2 !== this.side3
+            ? 'is scalene'
+            : 'not scalene';
+    this.isEquilateral =
+        this.side1 === this.side2 && this.side1 === this.side3
+            ? 'is equilateral'
+            : 'not equilateral';
 }
 
 export default Triangle;
