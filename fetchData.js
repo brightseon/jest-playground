e27df96d7acc08f8642d3b8837cc3e93fceb6e8d
Seon <public.seon@gmail.com>
2022-06-30 14:06:14 +0900

    refactor: fetchData

--- fetchData.js
+++ fetchData.js
@@ -1,1 +1,10 @@
-export const fetchData = () => new Promise((resolve, reject) => resolve('peanut butter'));
+export const fetchData = (callback) =>
+    new Promise((resolve, reject) => {
+        if (callback) {
+            callback(null, 'peanut butter');
+            // callback('error', null);
+            return;
+        }
+        resolve('peanut butter');
+        // reject('error');
+    });
